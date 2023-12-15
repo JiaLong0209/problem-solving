6122ff1568fd4ea4e52dfe735705eb6e638fd675
JiaLong0209 <smt02091395@gmail.com>
2023-12-15 23:52:26 +0800

    231215 Solve sum of digits problem

---
+++ codewar/6kyu/sum_of_digits.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/541c8630095125aba6000c00/train/python
+
+# method 1
+def digital_root(n):
+    return n if len([*str(n)]) == 1 else digital_root(sum(map(lambda x: int(x), [*str(n)])))
+
+
